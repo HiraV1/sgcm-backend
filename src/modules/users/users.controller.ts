a7df2d19d1953af5a8671ce04631ca4f8d73af87
brenerms <brenermsilva@gmail.com>
2026-05-24 22:46:58 -0400

    Update users.controller.ts

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -159,7 +159,7 @@ export class UsersController {
   @HttpCode(HttpStatus.NO_CONTENT)
   @ApiOperation({ summary: 'Deactivate user by id' })
   @ApiResponse({
-    status: 200,
+    status: 204,
     description: 'User deactivated successfully',
   })
   @ApiResponse({
